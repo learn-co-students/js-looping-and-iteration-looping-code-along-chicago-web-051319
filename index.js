1b47d5372491773b49e829de88b5594c70213206
John Linatoc <johnlinatoc1227@gmail.com>
2019-06-24 09:45:14 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards(arr, wrd) {
+
+  const arr2 = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    arr2.push(`Thank you, ${arr[i]}, for the wonderful ${wrd} gift!`);
+  };
+
+  return arr2;
+}
+
+function countdown(num) {
+  let countdown = 0;
+  while(num >= countdown) {
+    console.log(num);
+    num--;
+  };
+};
