6556e6811ec4d1ededbc375a2dd6af92230621ce
Jooyon Won <jyon1204@gmail.com>
2019-06-24 10:21:45 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+function writeCards (name, eventName){
+  let arr = [];
+  for(let i = 0; i < name.length; i++){
+    arr.push(`Thank you, ${name[i]}, for the wonderful ${eventName} gift!`)
+  }
+  return arr;
+}
+
+writeCards()
+
+
+function countdown(count){
+    while (count >= 0){
+    console.log(count);
+    count--;
+  }
+}
+
+countdown()
